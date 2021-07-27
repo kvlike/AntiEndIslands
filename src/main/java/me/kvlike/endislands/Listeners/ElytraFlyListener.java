e0e6a735f90b1308bfca0a1579ca65b037f24796
Mikołaj Kowalik <74322367+kvlike@users.noreply.github.com>
2021-07-27 13:39:42 +0200

    .

--- src/main/java/me/kvlike/endislands/Listeners/ElytraFlyListener.java
+++ src/main/java/me/kvlike/endislands/Listeners/ElytraFlyListener.java
@@ -1,9 +1,6 @@
 package me.kvlike.endislands.Listeners;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -14,13 +11,11 @@ public class ElytraFlyListener implements Listener {
     public void onElytra(PlayerMoveEvent e) {
         if (!e.getPlayer().hasPermission("end")) {
             if (e.getPlayer().isGliding() && e.getPlayer().getInventory().getChestplate().getType() == Material.ELYTRA) {
-                Location spawn = new Location(Bukkit.getWorld("world").getSpawnLocation());
-                e.getPlayer().teleport(spawn);
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tempban " + e.getPlayer().getName() + " 72h Elytra");
+                e.getPlayer().kickPlayer(ChatColor.RED + "Elytra jest zakazana!");
             } else if (e.getPlayer().getWorld().getEnvironment() == World.Environment.THE_END && (e.getPlayer().getLocation().getX() > 800.0 || e.getPlayer().getLocation().getZ() > 800.0 || e.getPlayer().getLocation().getX() < -800.0 || e.getPlayer().getLocation().getZ() < -800.0)) {
                 Location spawn = new Location(Bukkit.getWorld("world").getSpawnLocation());
                 e.getPlayer().teleport(spawn);
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tempban " + e.getPlayer().getName() + " 72h Proba dostania sie na wyspy endu");
+                e.getPlayer().sendMessage(ChatColor.RED + "Wyspy endu sa zakazane!");
             }
         }
     }
